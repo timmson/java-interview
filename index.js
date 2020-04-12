2735cc62d23ca650136e926082bf21105060e0a5
timmson <timmson666@mail.ru>
2020-04-12 18:12:22 +0300

    Fix some mistakes

--- index.js
+++ index.js
@@ -1,55 +1,88 @@
 const fs = require("./lib/fs");
 const pr = require("./lib/parser");
+const Speller = require("./lib/speller");
 require("colors");
 
 const EOL = "\r\n";
 const rootFileName = "README.md";
 const dirName = __dirname;
+const sp = new Speller(JSON.parse(fs.readFile(dirName + "/.yaspellerrc")));
 
-console.log("Files generation:");
+console.log("File spellcheking and generation:");
 
 function log(fileName, startTime) {
-    console.log("✓".green + " " + dirName + "/" + fileName + " " + ((new Date().getTime() - startTime) + " ms").magenta);
+	console.log("✓".green + " " + dirName + "/" + fileName + " " + ((new Date().getTime() - startTime) + " ms").magenta);
 }
 
+function logError(fileName, startTime, reason) {
+	console.log("✗".red + " " + dirName + "/" + fileName + " " + ((new Date().getTime() - startTime) + " ms").magenta);
+	console.log("-----".red);
+	console.log(JSON.stringify(reason, null, 2).red);
+	console.log("-----".red);
+}
+
+let promises = [];
+
 const mdFilesContents = fs.getListOfMDFiles(dirName)
-    .map((mdFile) => {
-            let startTime = new Date().getTime();
-            let fileContent = fs.readFileContent(mdFile);
-            let articleName = pr.getArticleName(fileContent);
-            let questions = pr.getQuestions(fileContent);
+	.map((mdFile) => {
+			let startTime = new Date().getTime();
+			let fileSource = fs.readFile(mdFile);
 
-            fs.writeFileContent(
-                mdFile,
-                pr.replaceTableOfContent(fileContent, questions.map(q => pr.mapHeaderToLink(q))).join(EOL)
-            ).then(() => log(mdFile, startTime)).catch(e => console.error(e));
+			async function f() {
 
-            return {
-                fileName: mdFile,
-                articleName: articleName,
-                questions: questions
-            }
-        }
-    );
+			}
 
-let readMeContents = [];
-readMeContents.push("# Вопросы для собеседования на Java Developer");
+			promises.push(sp.check(fileSource));
 
-readMeContents.push("");
+			let fileContent = pr.splitIntoLines(fileSource);
+			let articleName = pr.getArticleName(fileContent);
+			let questions = pr.getQuestions(fileContent);
 
-mdFilesContents.forEach(mdFileContent => readMeContents.push(pr.mapHeaderToLink(mdFileContent.articleName)));
+			promises.push(fs.writeFileContent(
+				mdFile,
+				pr.replaceTableOfContent(fileContent, questions.map(q => pr.mapHeaderToLink(q))).join(EOL)
+			));
 
-readMeContents.push("");
+			log(mdFile, startTime);
 
-mdFilesContents.forEach((mdFileContent) => {
-    readMeContents.push("## " + mdFileContent.articleName);
+			return {
+				fileName: mdFile,
+				articleName: articleName,
+				questions: questions
+			}
+		}
+	);
 
-    mdFileContent.questions.forEach(q => readMeContents.push(pr.mapHeaderToLink(q, mdFileContent.fileName)));
-    readMeContents.push("");
+Promise.all(promises).then(values => {
 
-    readMeContents.push("[к оглавлению](#Вопросы-для-собеседования-на-java-developer)");
-    readMeContents.push("");
-});
+	let err = values.filter(v => v && v.length > 0);
+	err.forEach(v => logError("", 0, v));
 
-let startTime = new Date().getTime();
-fs.writeFileContent(rootFileName, readMeContents.join(EOL)).then(() => log(rootFileName, startTime)).catch(e => console.error(e));
+	if (err.length > 0) {
+		process.exit(1);
+	}
+
+	let readMeContents = [];
+	readMeContents.push("# Вопросы для собеседования на Java Developer");
+
+	readMeContents.push("");
+
+	mdFilesContents.forEach(mdFileContent => readMeContents.push(pr.mapHeaderToLink(mdFileContent.articleName)));
+
+	readMeContents.push("");
+
+	mdFilesContents.forEach((mdFileContent) => {
+		readMeContents.push("## " + mdFileContent.articleName);
+
+		mdFileContent.questions.forEach(q => readMeContents.push(pr.mapHeaderToLink(q, mdFileContent.fileName)));
+		readMeContents.push("");
+
+		readMeContents.push("[к оглавлению](#Вопросы-для-собеседования-на-java-developer)");
+		readMeContents.push("");
+	});
+
+	let startTime = new Date().getTime();
+	fs.writeFileContent(rootFileName, readMeContents.join(EOL)).then(() => log(rootFileName, startTime)).catch(e => console.error(e));
+})
+
+
--- test/test-speller.js
+++ test/test-speller.js
@@ -2,18 +2,19 @@ const Speller = require("../lib/speller");
 const {expect} = require("chai");
 require("mocha");
 
-const speller = new Speller("../.yaspellerrc");
+//const settings = JSON.parse(require("fs").readFileSync(__dirname + "/../.yaspellerrc", "utf8"));
+const settings = {"lang": "ru", "checkYo": true, "dictionary": ["сплитератор.*"]}
+const speller = new Speller(settings);
 
 describe("Speller should", () => {
-/*	it("return split file content into lines", async () => {
-		const arrange = "Потомучта Сплитератор";
+	it("return split file content into lines", async () => {
+		const arrange = "Потомучта Сплитератором ее";
 
 		const result = await speller.check(arrange);
 
-		console.log(result);
-
-		expect(result).is.equal("1");
-
-	});*/
+		expect(result).has.length(2);
+		expect(result[0].word).is.equal("Потомучта");
+		expect(result[1].word).is.equal("ее");
+	});
 
 });
