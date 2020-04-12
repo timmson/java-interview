1f180925ab76cbffe9e4f3f3e695357c7c6efd7c
timmson <timmson666@mail.ru>
2020-04-12 15:38:05 +0300

    Add Spliterator

---
+++ test/test-speller.js
@@ -0,0 +1,19 @@
+const Speller = require("../lib/speller");
+const {expect} = require("chai");
+require("mocha");
+
+const speller = new Speller("../.yaspellerrc");
+
+describe("Speller should", () => {
+/*	it("return split file content into lines", async () => {
+		const arrange = "Потомучта Сплитератор";
+
+		const result = await speller.check(arrange);
+
+		console.log(result);
+
+		expect(result).is.equal("1");
+
+	});*/
+
+});
